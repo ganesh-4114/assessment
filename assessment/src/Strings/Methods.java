a935eaf323ebbf80392edc24247c9106e5872ba8
Ganesh Chowdary Dandamudi <84732406+ganesh-4114@users.noreply.github.com>
2021-12-23 10:35:36 +0530

    assessment

---
+++ assessment/src/Strings/Methods.java
@@ -0,0 +1,26 @@
+package Strings;
+
+public class Methods {
+	public static void main(String args[]){  
+		
+		//charAt method
+		String name="javatpoint";  
+		char ch=name.charAt(4);  
+		System.out.println("charAt: "+ch + "\n");  
+		
+		//compareTo
+		String nu1 = "Hello Java";
+		System.out.println("CompareTo " + name.compareTo(nu1) + "\n");
+		
+		//conatins
+		System.out.println("Contains: " + name.contains("java") + "\n");
+		
+		//endsWith
+		System.out.println("endsWith: " +   name.endsWith("int") + "\n");
+		
+		//
+		
+		
+		
+		}
+}
